08594efea3df89a10771e18ede2618896db75dcb
Alireza Alizadeh <alirezaa025@gmail.com>
2022-05-04 11:08:30 +0430

    create class for connection to db

---
+++ controllers/dbConnection.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class MySqlDatabaseConnection
+{
+    private static $instance = null;
+    private PDO $conn;
+
+
+    private function __construct()
+    {
+        $this->conn = new PDO("mysql:host=localhost;dbname=maktab", 'root', '');
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new MySqlDatabaseConnection();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->conn;
+    }
+
+    public function closeConnection()
+    {
+        $this->conn = null;
+    }
+}
